d988aea65da6f144edc6a52f91fbecdf0327ae3a
Igor Gomes <igorgmsf@gmail.com>
2020-08-01 22:26:38 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  { useEffect, useState } from "react";
+import axios from 'axios';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faShoppingCart,
+  faUser,
+  faStar
+} 
+from "@fortawesome/free-solid-svg-icons";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from "react-responsive-carousel";
+
+import CorebizLogo from "./components/logo";
+
+
 
 function App() {
+
+const [products, setProducts] = useState([])
+
+useEffect(() => {
+  axios
+    .get("https://corebiz-test.herokuapp.com/api/v1/products")
+    .then((response) => {
+      setProducts(response.data) 
+    });
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <CorebizLogo />
+        <div className="search">
+          <input placeholder="O que está procurando?"></input>
+          <a href="/">
+            <FontAwesomeIcon icon={faSearch} />
+          </a>
+        </div>
+        <nav>
+          <ul className="menu">
+            <li>
+              <div className="account">
+                <a href="/">
+                  <FontAwesomeIcon icon={faUser} />
+                  Minha conta
+                </a>
+              </div>
+            </li>
+            <li>
+              <a href="/">
+                <FontAwesomeIcon icon={faShoppingCart} />
+              </a>
+            </li>
+          </ul>
+        </nav>
       </header>
+
+      <section>
+        <Carousel showThumbs={false}>
+          <div className="maskgroup">
+            <div className="banner"></div>
+            <div className="banner2">
+              <div className="img-carousel"></div>
+            </div>
+          </div>
+          <div className="maskgroup">
+            <div className="banner"></div>
+            <div className="banner2">
+              <div className="img-carousel"></div>
+            </div>
+          </div>
+          <div className="maskgroup">
+            <div className="banner"></div>
+            <div className="banner2">
+              <div className="img-carousel"></div>
+            </div>
+          </div>
+        </Carousel>
+      </section>
+
+      <h1>Mais vendidos</h1>
+
+      <section className="prateleira">
+        {products.map((product) => (
+          <div key={product.productId} className="card">
+            <img src={product.imageUrl}></img>
+            <div className="description">
+              <p>{product.productName}</p>
+              {[...Array(5).keys()].map((item) => (
+                <FontAwesomeIcon key={item} icon={faStar} />
+              ))}
+              {product.listPrice && <p className="listprice">{`de ${product.listPrice.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})}`}</p>}
+              <p>{product.price}</p>
+              {product.installments.length > 0 &&
+                product.installments.map((installment) => (
+                  <p>
+                    ou em {installment.quantity}x de R$ {installment.value}
+                  </p>
+                ))}
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
---
+++ src/components/logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from "../assets/logo.svg";
+
+const CorebizLogo = () => {
+    return <a href="/"><img src={logo} alt ="Corebiz logo"></img></a>
+}
+
+export default CorebizLogo
+
